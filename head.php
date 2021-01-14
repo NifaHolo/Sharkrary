0e6ad62efd611cd33fb4390e9a8ef369c4d5d2ce
NifHolo <39643916+NifaHolo@users.noreply.github.com>
2021-01-14 12:15:29 +0100

    Delete head.php

--- head.php
+++
@@ -1,6 +0,0 @@
-		<?php require "config.php"; ?>
-	<link rel="stylesheet" href="<?php echo $CONFIG['root_path'] ?>style/style.css">
-	<title>Sharkrary</title>
-	<meta charset="UTF-8">
-
-  <link rel="icon" href="<?php echo $CONFIG['root_path'] ?>images/logo_sharkrary.png">
